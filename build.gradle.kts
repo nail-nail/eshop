4731bf9a39da4a1aa71afe2d642b257c471fdaae
nail-nail <naailakhadijah@gmail.com>
2026-02-21 15:47:42 +0700

    changed build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -65,4 +66,15 @@ tasks.register<Test>(name = "functionalTest") {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+tasks.test {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
 }
