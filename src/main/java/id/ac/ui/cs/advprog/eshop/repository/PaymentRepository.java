1013d7f626a3738990afb59766dd7071c9edfdb9
nail-nail <naailakhadijah@gmail.com>
2026-03-10 17:56:36 +0700

    [REFACTOR] Use HashMap for PaymentRepository storage

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -4,35 +4,25 @@ import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> paymentData = new ArrayList<>();
+
+    private Map<String, Payment> paymentData = new HashMap<>();
 
     public Payment save(Payment payment) {
-        int i = 0;
-        for (Payment saved : paymentData) {
-            if (saved.getId().equals(payment.getId())) {
-                paymentData.set(i, payment);
-                return payment;
-            }
-            i++;
-        }
-        paymentData.add(payment);
+        paymentData.put(payment.getId(), payment);
         return payment;
     }
 
     public Payment getPayment(String id) {
-        for (Payment saved : paymentData) {
-            if (saved.getId().equals(id)) {
-                return saved;
-            }
-        }
-        return null;
+        return paymentData.getOrDefault(id, null);
     }
 
     public List<Payment> getAllPayments() {
-        return new ArrayList<>(paymentData);
-    }
+        return new ArrayList<>(paymentData.values());
+}
 }
